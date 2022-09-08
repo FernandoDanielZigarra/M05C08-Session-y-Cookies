b80e745a2731598cdf76c2f932baf9438b802658
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-09-08 17:27:30 -0300

    session y cookies finalizado

---
+++ src/validations/validator.js
@@ -0,0 +1,16 @@
+const {body} = require('express-validator');
+
+module.exports = [
+    body('nombre')
+        .notEmpty().withMessage('Por favor, ingresa tu nombre.'),
+
+    body('colors')
+        .notEmpty().withMessage('Por favor, ingresa tu color.'),
+
+    body('email')
+        .notEmpty().withMessage('Por favor, ingresa tu mail.'),
+
+    body('edad')
+        .notEmpty().withMessage('Por favor, ingresa tu edad.').bail()
+        .isNumeric().withMessage('Ingrese un valor numerico')
+]
